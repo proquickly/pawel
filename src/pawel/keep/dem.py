89385bc788559dc8baf173592b9c7e15561ec049
Andy Miles <akmiles@icloud.com>
2025-02-22 08:29:41 -0800

    cleanup

--- src/pawel/keep/dem.py
+++
@@ -1,5 +0,0 @@
-def addup(number1: int, number2: int) -> int:
-    return number1 + number2
-
-
-print(addup("a", "b"))
--- src/pawel/keep/iterloop.py
+++
@@ -1,6 +0,0 @@
-numbers = [1,2,3,4,5, 100]
-for number in numbers:
-    print(number)
-    numbers.append(number+1)
-    if number > 100:
-        break
